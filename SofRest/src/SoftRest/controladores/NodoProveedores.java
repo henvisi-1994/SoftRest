e6eb69057b233cba190153f2c6bb2bff6a8a1e55
usuario <usuario@PaulTorres>
2017-07-19 02:15:33 -0500

    Lista Proveedores creada

--- SofRest/src/SoftRest/controladores/NodoProveedores.java
+++ SofRest/src/SoftRest/controladores/NodoProveedores.java
@@ -1,22 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SoftRest.controladores;
- 
+
 /**
  *
- * @author Eddie Bustamante
+ * @author Paul Torres
  */
-    public class NodoProveedores<T> {
+public class NodoProveedores<T> {
 
-        public T info;
-        public NodoProveedores sgte;
+    public T info;
+    public NodoProveedores sgte;
 
-        public NodoProveedores(T info) {
-            this.info = info;
-            this.sgte = null;
-        }
-    
+    public NodoProveedores(T info) {
+        this.info = info;
+        this.sgte = null;
+    }
 }
---
+++ SofRest/src/SoftRest/controladores/Proveedores.java
@@ -0,0 +1,69 @@
+package SoftRest.controladores;
+
+/**
+ *
+ * @author Eddie Bustamante
+ */
+public class Proveedores {
+    protected String ruc;
+    protected String nombre; 
+    protected String telefono;
+    protected String email;
+    protected String direccion;
+    
+    public Proveedores(String ruc, String nombre, String telefono, String email, String direccion) {
+        this.ruc = ruc;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+        }
+    public Proveedores(){
+        
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String cedula) {
+        this.ruc = ruc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String ImprimirProveedor() {
+        return "[ " + ruc + "\t" + nombre + "\t" + email + "\t" + direccion + "\t"+ telefono ;
+    }
+    
+}
